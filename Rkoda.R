59129d24976d3c9d8c6156629e52ff442403ad1c
janaobsteter <obsteter.jana@gmail.com>
2020-10-13 09:11:38 +0200

    work tecaj

---
+++ Rkoda.R
@@ -0,0 +1,55 @@
+for (dan in 1:10) {
+  print(paste0("Danes je ", dan, ". september."))
+}
+
+# Inicializiramo spremenljivko
+vsota <- 0
+# Začnemo zanko, določimo iteracijo
+for (stevilka in 1:10) {
+  # Posodobimo spremenljivko "vsota"
+  vsota <- vsota + stevilka
+}
+
+# Inicializiramo spremenljivko "stevilo"
+stevilo <- 0
+# Začnemo zanko, določimo končni pogoj
+while (stevilo < 10) {
+  # Izpiši število
+  print(paste0("To je število ", stevilo))
+  # Posodobimo spremenljivko "stevilo"
+  stevilo <- stevilo + 1
+}
+
+# Inicializiramo spremenljivko "stevilo"
+stevilo <- 0
+if (stevilo %/% 2 == 0) {
+  print(paste0("Število ", stevilo, " je sodo.")
+} 
+
+
+stevilo <- 0
+if (stevilo %/% 2 == 0) {
+  print(paste0("Število ", stevilo, " je sodo.")
+} else {
+  print(paste0("Število ", stevilo, " ni sodo.")
+}
+
+# Inicializiraj spremenljivko "stevilo"
+stevilo <- 0
+# Če je ostanek pri deljenju z dva nič
+if (stevilo %/% 2 == 0) {
+  # Izpiši, da je število sodo
+  print(paste0("Število ", stevilo, " je sodo.")
+} else if (stevilo %/% 2 == 1) { # Če je ostanek ena
+  # Izpiši
+  print(paste0("Število ", stevilo, " je liho")
+} else {
+  print(paste0("Število ", stevilo, " ni sodo ne liho.")
+}
+
+
+a <- array(c(1,2,3), dim=c(3,3,2))
+a
+
+
+
